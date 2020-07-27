480b15ed3cc9138f592ae82a29eb0e4b73a8d3ad
MoeRayo <moronfolu.motunrayo1@gmail.com>
2020-07-27 19:03:57 +0100

    feat: authentication of user email input

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 let subscribeButton = document.getElementById("arrow-icon");
 let textBox = document.getElementById("email");
+let errorText = document.getElementById("error-text");
+let errorIcon = document.getElementById("error-icon")
+let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+subscribeButton.addEventListener('click', function(){
+    if (textBox.value == "" || !textBox.value.match(mailFormat)){
+        errorText.style.display ="block";
+        errorIcon.style.display ="block";
+
+    } else {
+        errorText.style.display ="none";
+        errorIcon.style.display ="none";
+    }
+})
